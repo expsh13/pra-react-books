e15956d01b793053e3f20f032f3c2604c23d9895
畑怜輝 <gm4sato8.sopcacienrniisan@gmail.com>
2022-12-31 10:38:01 +0900

    no message

--- htdocs/src/hooks/useMemoList.ts
+++ htdocs/src/hooks/useMemoList.ts
@@ -6,17 +6,23 @@ import { IndentStyle } from "typescript";
 export const useMemoList = () => {
   const [memos, setMemos] = useState<string[]>([]);
 
-  const addTodo = useCallback((text: string) =>{
-    const newMemos = [...memos];
-    newMemos.push(text);
-    setMemos(newMemos);
-  },[memos]);
+  const addTodo = useCallback(
+    (text: string) => {
+      const newMemos = [...memos];
+      newMemos.push(text);
+      setMemos(newMemos);
+    },
+    [memos]
+  );
 
-  const deleteTodo = useCallback((index: number) => {
-    const newMemos = [...memos];
-    newMemos.splice(index,1);
-    setMemos(newMemos);
-  },[memos]);
+  const deleteTodo = useCallback(
+    (index: number) => {
+      const newMemos = [...memos];
+      newMemos.splice(index, 1);
+      setMemos(newMemos);
+    },
+    [memos]
+  );
 
-  return { memos, addTodo, deleteTodo};
-}
+  return { memos, addTodo, deleteTodo };
+};
